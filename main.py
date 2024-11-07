16e549c669d5c2706ffe308f5773ecf9b437e208
Yashodhan Mandal <yashodhan.mandal.official@gmail.com>
2024-11-07 18:07:33 +0530

    created script to interact with the code

---
+++ main.py
@@ -0,0 +1,7 @@
+from generate import generate
+
+exam = input("Enter Exam: ")
+syllabus = input("Enter Syllabus: ")
+time = input("Enter Time: ")
+context = input("Enter Context: ")
+generate(syllabus=syllabus, exam=exam, time=time, context=context)
